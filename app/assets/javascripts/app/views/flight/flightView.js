5e89fd51d45c61f80754af31b033079b3de21ab8
Allen <0allen.hsu@gmail.com>
2014-07-31 15:16:43 +1000

    flights

--- app/assets/javascripts/app/views/flight/flightView.js
+++ app/assets/javascripts/app/views/flight/flightView.js
@@ -9,10 +9,5 @@ app.viewFlight = Backbone.View.extend({
 
   render: function() {
     this.$el.html(app.templates.flightView);
-    console.log(app.templates.flightView);
-
-    // var view_html = Handlebars.compile(app.templates.flightView);
-    // var copy = view_html();
-
   }
 })
