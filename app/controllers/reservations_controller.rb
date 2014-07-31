9aab17412ec3db4d45440b1de4dbf41272cd5827
lyntco <strawww@gmail.com>
2014-07-31 13:59:42 +1000

    Routes

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -4,8 +4,13 @@ class ReservationsController < ApplicationController
   end
 
   def create
+    reservation = Reservation.create(:user_id => @current_user.id, :flight_id => params[:flight_id])
+    render :json => reservation
   end
 
   def destroy
+    reservation = Reservation.find(params[:id])
+    reservation.destroy
+    render :text => "Boom."
   end
 end
