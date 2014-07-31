73483cc57b14f9009e0ebc74d7df801ad3ffcd3b
Allen <0allen.hsu@gmail.com>
2014-07-31 23:36:22 +1000

    show seats

--- app/assets/javascripts/app/views/airplane/airplaneShowView.js
+++ app/assets/javascripts/app/views/airplane/airplaneShowView.js
@@ -6,11 +6,11 @@ app.AirplaneShowView = Backbone.View.extend({
     this.render();
   },
   render: function() {
-    // var html = Handlebars.compile( app.templates.airplaneShowView );
-    // var copy = html( this.model.toJSON() );
-    // this.$el.html( copy );
-    // console.log(this)
-    // var seating = new app.SeatingView({model: this.model});
+    var html = Handlebars.compile( app.templates.airplaneShowView );
+    var copy = html( this.model.toJSON() );
+    this.$el.html( copy );
+    console.log(this)
+    var seating = new app.SeatingView({model: this.model});
   }
 
 });
