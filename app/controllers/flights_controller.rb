0b8700a63d312cad0ea63688e0202fc666c7a3fa
Allen <0allen.hsu@gmail.com>
2014-08-01 13:16:42 +1000

    sending user id

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -8,6 +8,7 @@ class FlightsController < ApplicationController
     response = {
       flight: Flight.find(params[:id]),
       seat: Reservation.where(flight_id: params[:id])
+      user_id: @current_user.id
     }
     render json: response
   end
